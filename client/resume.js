aaed094bb29194e691edf3cd65601bbeb87da535
Stefan Generalao <stefan.generalao@gmail.com>
2017-07-26 08:36:09 +0200

    Minor update

--- client/resume.js
+++ client/resume.js
@@ -2,29 +2,26 @@ const resume = () => {
   return {
     "Intro": {
       "Stefan Generalao": {
-        props: [],
-        subtitle: "content"
+        "props": [],
+        "subtitle": "content"
       }
     },
 
     "Arbetslivserfarenhet": {
-      "XTZ AB": {
+      "XTZ AB": [
         "Position": "Tekniker",
-        "Accomplishments": "bababa",
         "Location": null,
         "Date": null
-      },
+      ],
 
       "Espresso House AB": {
         "Position": null,
-        "Accomplishments": null,
         "Location": null,
         "Date": null
       },
 
       "Cherry Casino AB": {
         "Position": null,
-        "Accomplishments": null,
         "Location": null,
         "Date": null
       }
@@ -76,178 +73,112 @@ const resume = () => {
 
     "Skickligheter": {
       "First principle thinking": {
-        "Score": {
-          "slider": true,
-          "value": 0.5
-        },
+        "Score": 0.5,
         "Details": null
       },
 
       "Artificiell intelligens, genetiska algoritmer & maskininlärning": {
-        "Score": {
-          "slider": true,
-          "value": 0.8
-        },
+        "Score": 0.8,
         "Details": "Synapse.js, Neataptic.js, TensorFlow, Scikit-learn"
       },
 
       "P5JS": {
-        "Score": {
-          "slider": true,
-          "value": 1.0
-        },
+        "Score": 1.0,
         "Details": null
       },
 
       "Java Development": {
-        "Score": {
-          "slider": true,
-          "value": 0.5
-        },
+        "Score": 0.5,
         "Details": null
       },
 
       "JavaScript": {
-        "Score": {
-          "slider": true,
-          "value": 0.9
-        },
+        "Score": 0.9,
         "Details": null
       },
 
       "Python": {
-        "Score": {
-          "slider": true,
-          "value": 0.3
-        },
+        "Score": 0.3,
         "Details": null
       },
 
       "Arduino (Mjukvara)": {
-        "Score": {
-          "slider": true,
-          "value": 1.0
-        },
+        "Score": 1.0,
         "Details": null
       },
 
       "Arduino (Hårdvara)": {
-        "Score": {
-          "slider": true,
-          "value": 1.0
-        },
+        "Score": 1.0,
         "Details": null
       },
 
       "C/C++ (Inbyggda system)": {
-        "Score": {
-          "slider": true,
-          "value": 0.7
-        },
+        "Score": 0.7,
         "Details": "ARM Cortex-m3"
       },
 
       "Raspberry PI": {
-        "Score": {
-          "slider": true,
-          "value": 0.7
-        },
+        "Score": 0.7,
         "Details": null
       },
 
       "UNIX-terminal & UNIX-based systems": {
-        "Score": {
-          "slider": true,
-          "value": 0.8
-        },
+        "Score": 0.8,
         "Details": null
       },
 
       "Penetration testing": {
-        "Score": {
-          "slider": true,
-          "value": 0.2
-        },
+        "Score": 0.2,
         "Details": null
       },
 
       "ReactJS": {
-        "Score": {
-          "slider": true,
-          "value": 0.6
-        },
+        "Score": 0.6,
         "Details": "Redux"
       },
 
       "Firebase": {
-        "Score": {
-          "slider": true,
-          "value": 0.3
-        },
+        "Score": 0.3,
         "Details": null
       },
 
       "Webpack": {
-        "Score": {
-          "slider": true,
-          "value": 0.2
-        },
+        "Score": 0.2,
         "Details": null
       },
 
       "NodeJS": {
-        "Score": {
-          "slider": true,
-          "value": 0.3
-        },
+        "Score": 0.3,
         "Details": null
       },
 
       "HTML": {
-        "Score": {
-          "slider": true,
-          "value": 0.4
-        },
+        "Score": 0.4,
         "Details": null
       },
 
       "CSS": {
-        "Score": {
-          "slider": true,
-          "value": 0.3
-        },
+        "Score": 0.3,
         "Details": null
       },
 
       "Adobe Photoshop": {
-        "Score": {
-          "slider": true,
-          "value": 0.7
-        },
+        "Score": 0.7,
         "Details": null
       },
 
       "3D-printing": {
-        "Score": {
-          "slider": true,
-          "value": 1.0
-        },
+        "Score": 1.0,
         "Details": "Kunskap om möjligheter, begränsningar och tekniker med 3d-printing."
       },
 
       "CAD": {
-        "Score": {
-          "slider": true,
-          "value": 0.5
-        },
+        "Score": 0.5,
         "Details": "Solidworkds, Autocad Inventor, OnShape"
       },
 
       "Simplify3D": {
-        "Score": {
-          "slider": true,
-          "value": 0.9
-        },
+        "Score": 0.9,
         "Details": "Styrprogram för 3D-skrivare."
       }
     },
