04def278d9c6448b4da923fc590666941c1780cc
Stefan Generalao <stefan.generalao@gmail.com>
2017-08-08 20:41:43 +0200

    Code cleaning, no fixes or new features

--- client/app/body.js
+++ client/app/body.js
@@ -5,95 +5,101 @@ import Divider from 'material-ui/Divider'
 import LinearProgress from 'material-ui/LinearProgress'
 
 class Body extends Component {
+  // Function returning an array of cards with content.
   createCards() {
-    // Return if selectedItem is null
-    if (!this.props.selectedItem)
-      return
+    // Return null if selectedItem is null.
+    if (!this.props.selectedItem) return null
 
+    // Selected section, e.g. "Intro", "Skills", "Experience".
     const selectedSection = resume[this.props.selectedItem]
-    const cards = Object.keys(selectedSection).map((cardTitle) => {
-      const item = selectedSection[cardTitle]
+
+    // Array of cards with content.
+    const cards = Object.keys(selectedSection).map((cardIndex) => {
+      const lineCommandValuePair = selectedSection[cardIndex]
 
       const createContent = () => {
-        const content = item.map((line, i) => {
-          const lineKey = Object.keys(line)[0]
-          const lineValue = line[lineKey]
+        const content = lineCommandValuePair.map((line, i) => {
+          const lineCommand = Object.keys(line)[0]
+          const lineValue = line[lineCommand]
 
-          // Subtitle
-          if (lineKey == "subtitle") {
-            return (
-              <div key={content+lineKey+lineValue+i}>
-                <CardText
+          // Return content depending on lineCommand.
+          switch (lineCommand) {
+            case "subtitle":
+              return (
+                <div key={content+lineCommand+lineValue+i}>
+                  <CardText
+                    style={{
+                      paddingBottom: "0px",
+                      paddingTop: "30px"
+                    }}
+                  >
+                    <b>{lineValue}</b>
+                  </CardText>
+                </div>
+              )
+
+            case "text":
+              return (
+                <div key={content+lineCommand+lineValue+i}>
+                  <CardText
+                    style={{
+                      "paddingTop": "6px",
+                      "paddingBottom": "6px"
+                    }}
+                  >
+                    {lineValue}
+                  </CardText>
+                </div>
+              )
+
+            case "title":
+              return (
+                <div key={content+lineCommand+lineValue+i}>
+                  <CardTitle title={lineValue}/>
+                  <Divider/>
+                </div>
+              )
+
+            case "progressbar":
+              return (
+                <div
+                  key={content+lineCommand+lineValue+i}
                   style={{
-                    paddingBottom: "0px",
-                    paddingTop: "30px"
+                    padding: "15px",
+                    maxWidth: "400px"
                   }}
                 >
-                  <b>{lineValue}</b>
-                </CardText>
-              </div>
-            )
+                  <LinearProgress
+                    mode="determinate"
+                    value={lineValue}
+                  />
+                </div>
+              )
 
-            // Text
-          } else if (lineKey == "text") {
-            return (
-              <CardText
-                key={content+lineKey+lineValue+i}
-                style={{
-                  "paddingTop": "6px",
-                  "paddingBottom": "6px"
-                }}
-              >
-                {lineValue}
-              </CardText>
-            )
+            case "link":
+              return (
+                <div key={content+lineCommand+lineValue+i}>
+                  link:{lineValue}
+                </div>
+              )
 
-            // Title
-          } else if (lineKey == "title") {
-            return (
-              <div key={content+lineKey+lineValue+i}>
-                <CardTitle title={lineValue}/>
-                <Divider/>
-              </div>
-            )
+            default:
+              console.log(
+                "WARNING: Unknown lineCommand: " + lineCommand
+              )
 
-            // Progressbar
-          } else if (lineKey == "progressbar") {
-            return (
-              <div
-                key={content+lineKey+lineValue+i}
-                style={{
-                  padding: "15px",
-                  maxWidth: "400px"
-                }}>
-                <LinearProgress
-                  mode="determinate"
-                  value={lineValue}
-                />
-              </div>
-            )
-
-            // Link
-          } else if (lineKey == "link") {
-            return (
-              <div key={content+lineKey+lineValue+i}>
-                link:{lineValue}
-              </div>
-            )
-
-            // Should never occur
-          } else {
-            console.log("unknown lineKey")
-            return null
-          }
-        })
+              return (
+                <div key={content+lineCommand+lineValue+i} />
+              )
+            }
+          })
 
         return content
       }
 
       const card = (
         <Card
-          key={cardTitle}
+          key={cardIndex}
           style={{
             margin: "10px",
             paddingBottom: "20px"
@@ -104,7 +110,7 @@ class Body extends Component {
       )
 
       return card
-    });
+    })
 
     return cards
   }
