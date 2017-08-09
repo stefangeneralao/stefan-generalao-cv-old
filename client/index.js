43b17e7436d9d3fca0c56b0f1b8404653ac9463b
Stefan Generalao <stefan.generalao@gmail.com>
2017-08-09 09:43:56 +0200

    Added darkBaseTheme-option

--- client/index.js
+++ client/index.js
@@ -3,18 +3,18 @@ import ReactDOM from 'react-dom'
 import App from './app/app'
 import {MuiThemeProvider} from 'material-ui'
 import injectTapEventPlugin from 'react-tap-event-plugin'
-injectTapEventPlugin()
-import {Provider} from 'react-redux'
-import {createStore} from 'redux'
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
 import * as colors from 'material-ui/styles/colors'
 import resume from './resume'
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme'
 
+injectTapEventPlugin()
 window.resume = resume()
 
 // Render application within MuiThemeProvider from material-ui
 // and within Provider from Redux.
 ReactDOM.render(
+  // <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
   <MuiThemeProvider>
     <App />
   </MuiThemeProvider>,
